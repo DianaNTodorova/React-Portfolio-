beec3749d0f18c1885ef248e84548efff602ea27
Diana Todorova <diana.n.todorova@gmail.com>
2025-05-13 15:27:56 +0200

    `Header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const goToProjects = () => {
+    if (location.pathname !== "/") {
+      navigate("/", { replace: false });
+      setTimeout(() => {
+        document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" });
+      }, 100); 
+    } else {
+      document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <header>
       <div className="container" style={{ backgroundColor: '#08002e' }}>
@@ -23,7 +37,12 @@ const Header = () => {
               <Link to="/about">About Me</Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+            <button onClick={goToProjects} style={{ all: 'unset', cursor: 'pointer',}}>
+             Projects
+            </button>
+             </li>
+            <li>
+              <Link to="/contact">Contact</Link> 
             </li>
           </ul>
         </div>
